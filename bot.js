f3fa01224e1fd6ba1207ae6d141a2cb191801100
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-24 20:00:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2735,12 +2735,12 @@ client.on("guildMemberAdd", member => {
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
-                 ctx.arc(120.8, 120.5, 112.3, 0, Math.PI*2, true);
+                 ctx.arc(120.8, 120.5, 110.3, 0, Math.PI*2, true);
                    ctx.closePath();
                    
                                  ctx.clip();
  
-                       ctx.drawImage(ava, 5, 17, 227, 225);
+                       ctx.drawImage(ava, 7, 8, 227, 225);
                               ctx.closePath();
 
                             
