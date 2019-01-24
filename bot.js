71eff020dadf9546697942eac9a62b3a4a63b299
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-24 21:04:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2757,7 +2757,26 @@ client.on("guildMemberAdd", member => {
 
 
 
+client.on('ready', () => {
+  wait(1000);
 
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "اسم روم الولكم");
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+  });
+});
 
 
 
