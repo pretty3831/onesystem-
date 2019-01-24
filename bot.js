da553e69e551da08a47592b489ff2ecfa961e556
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-24 20:43:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2800,7 +2800,7 @@ client.on('guildMemberAdd', member => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const logChannel = member.guild.channels.find(channel => channel.name === "one");
-    logChannel.send **Invited by:** <@${inviter.id}>`);
+    logChannel.send Invited by: <@${inviter.id}>`);
   });
 });
 
