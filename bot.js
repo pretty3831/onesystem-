16d882e27ed9990d820a9727a4f03d83673f40fb
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-24 19:09:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2698,7 +2698,7 @@ client.on("guildMemberAdd", member => {
           var Canvas = require('canvas')
       var jimp = require('jimp')
       
-      const w = ['PicsArt_07-17-07.58.02 (1).png'];
+      const w = ['PicsArt_01-19-03.41.54.png'];
       
               let Image = Canvas.Image,
                   canvas = new Canvas(557, 241),
@@ -2735,7 +2735,7 @@ client.on("guildMemberAdd", member => {
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
-                 ctx.arc(191, 191, 112.3, 0, Math.PI*2, true);
+                 ctx.arc(120, 120, 112.3, 0, Math.PI*2, true);
                    ctx.closePath();
                    
                                  ctx.clip();
